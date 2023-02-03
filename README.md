de472ada92b81a699a469d425108376ca19303a3
IvanXablin <amdandpc67890@gmail.com>
2023-02-03 17:45:55 +0700

    init project

---
+++ README.md
@@ -0,0 +1,1 @@
+# dropvacancy-client
---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+ <el-button>Hello World</el-button>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
